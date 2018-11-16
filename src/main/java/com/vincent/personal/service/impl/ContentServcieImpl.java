33f04d1dba7f40bd048b17c995fe29fa9e0aeac8
zfy-zhang <zfy_zang@163.com>
2018-11-16 22:50:18 +0800

    关于redisTemplate操作redis键乱码问题解决

--- src/main/java/com/vincent/personal/service/impl/ContentServcieImpl.java
+++ src/main/java/com/vincent/personal/service/impl/ContentServcieImpl.java
@@ -20,8 +20,11 @@ import com.vincent.personal.util.RedisKeyUtil;
 import com.vincent.personal.util.Tools;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -48,8 +51,17 @@ public class ContentServcieImpl implements IContentService {
     @Resource
     private IMetaService metasService;
 
-    @Autowired
+    @Autowired(required = false)
     private RedisTemplate redisTemplate;
+
+    @Bean
+    public RedisTemplate redisTemplateInit() {
+        //设置序列化Key的实例化对象
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置序列化Value的实例化对象
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
 
 //    @Autowired
     @Resource(name = "redisTemplate")
